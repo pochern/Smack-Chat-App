395be27167e606c7deecb3af76c4d03173e1afbd
Polina Chernomaz <pchernomaz@polinas-mbp.fios-router.home>
2018-02-11 03:23:59 -0500

    Added 3 pods: Alamofire, SwiftJSON, Socket.IO framework via Cocoapods Added 4 Model-View-Controller Supporting Files: Supporting Files, Controller, View, Model 2 New View Controllers, Top and bottom 2 New Controller files, ChatVC and ChannelVC Added UIView + Label + Button to ChatVC Gave button action Added Gesture recognizers Customized RearViewRevealWidth

---
+++ Controller/ChatVC.swift
@@ -0,0 +1,39 @@
+//
+//  ChatVC.swift
+//  Smack Chat App
+//
+//  Created by Polina Chernomaz on 2/11/18.
+//  Copyright © 2018 Polina Chernomaz. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    //outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            self.view.addGestureRecognizer(revealViewController().tapGestureRecognizer())
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
