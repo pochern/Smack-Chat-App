f262da8e301f34874c9ded01a567448f135b7f0f
Polina Chernomaz <pchernomaz@polinas-mbp.fios-router.home>
2018-02-11 01:33:22 -0500

    Initial Commit

---
+++ Smack Chat App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack Chat App
+//
+//  Created by Polina Chernomaz on 2/11/18.
+//  Copyright © 2018 Polina Chernomaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
